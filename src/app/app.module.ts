160b1683a006194373666e3a73a9e41f2aa4e78f
CassieV <obicassievirtue@gmail.com>
2023-03-12 18:44:27 +0100

    Create project and add skeleton page

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddTodoComponent,
+    TodoListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
