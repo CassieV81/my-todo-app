13203d453a3f354651e7f814863f977cdc84f6ee
CassieV <obicassievirtue@gmail.com>
2023-03-13 17:16:50 +0100

    Add mark complete method

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -24,6 +24,6 @@ export class TodoListComponent {
     }
   }
   markComplete(todo: Todo) {
-
+    this.todoService.markComplete(todo);
   }
 }
--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -9,13 +9,13 @@ export class TodoService {
   constructor() { }
 
   todos: Todo[] = [
-    { id: 1, name: 'Buy groceries' },
-    { id: 2, name: 'Do laundry' },
-    { id: 3, name: 'Clean the house' }
+    { id: 1, name: 'Buy groceries', completed: false},
+    { id: 2, name: 'Do laundry', completed: true},
+    { id: 3, name: 'Clean the house', completed: false}
   ];
 
   addTodo(newTodoName: string) {
-    const newTodo: Todo = { id: this.todos.length + 1, name: newTodoName};
+    const newTodo: Todo = { id: this.todos.length + 1, name: newTodoName, completed: false};
     this.todos.push(newTodo);
   }
   editTodo(todo: Todo, newTodoName: string) {
@@ -27,5 +27,7 @@ export class TodoService {
         this.todos.splice(index, 1);
     }
   }
-
+  markComplete(todo: Todo) {
+    !todo.completed ? todo.completed = true : todo.completed = false;
+  }
 }
